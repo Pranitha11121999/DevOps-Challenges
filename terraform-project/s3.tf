d8ed14011fe2baba9516b34146932a21c5a18440
prani <kpranithaharani@gmail.com>
2025-01-31 19:32:49 -0800

    Fix S3 bucket ACL and add required permissions

--- terraform-project/s3.tf
+++ terraform-project/s3.tf
@@ -1,6 +1,5 @@
 resource "aws_s3_bucket" "model_bucket" {
   bucket = "deepseek-model-bucket-12345"  # Replace with a unique name
-  acl    = "private"
 
   tags = {
     Name        = "DeepSeekModelBucket"
@@ -8,7 +7,10 @@ resource "aws_s3_bucket" "model_bucket" {
   }
 }
 
-
+resource "aws_s3_bucket_acl" "model_bucket_acl" {
+  bucket = aws_s3_bucket.model_bucket.id
+  acl    = "private"
+}
 resource "aws_s3_bucket_server_side_encryption_configuration" "model_bucket_encryption" {
   bucket = aws_s3_bucket.model_bucket.id
 
